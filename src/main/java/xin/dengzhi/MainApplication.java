f6afcd9b10b554e06c26ac00ea9ff04155af720e
dengzhixin <786339231@qq.com>
2020-05-19 19:49:40 +0800

    init

---
+++ src/main/java/xin/dengzhi/MainApplication.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2016-2019 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package xin.dengzhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
---
+++ src/main/java/xin/dengzhi/modules/app/service/impl/UserServiceImpl.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2016-2019 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package xin.dengzhi.modules.app.service.impl;
+
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import xin.dengzhi.common.exception.RRException;
+import xin.dengzhi.common.validator.Assert;
+import xin.dengzhi.modules.app.dao.UserDao;
+import xin.dengzhi.modules.app.entity.UserEntity;
+import xin.dengzhi.modules.app.form.LoginForm;
+import xin.dengzhi.modules.app.service.UserService;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.stereotype.Service;
+
+
+@Service("userService")
+public class UserServiceImpl extends ServiceImpl<UserDao, UserEntity> implements UserService {
+
+	@Override
+	public UserEntity queryByMobile(String mobile) {
+		return baseMapper.selectOne(new QueryWrapper<UserEntity>().eq("mobile", mobile));
+	}
+
+	@Override
+	public long login(LoginForm form) {
+		UserEntity user = queryByMobile(form.getMobile());
+		Assert.isNull(user, "手机号或密码错误");
+
+		//密码错误
+		if(!user.getPassword().equals(DigestUtils.sha256Hex(form.getPassword()))){
+			throw new RRException("手机号或密码错误");
+		}
+
+		return user.getUserId();
+	}
+}
